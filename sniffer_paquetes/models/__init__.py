326a2e20165a8a6e802db35f12508e25cc75c486
RubenCA <ruben1000.ca@gmail.com>
2025-03-12 20:41:37 -0600

    docs: Se agrego documentacion extra sobre la finalidad del subpaquete unidades_de_datos

--- sniffer_paquetes/models/__init__.py
+++ sniffer_paquetes/models/__init__.py
@@ -2,4 +2,7 @@
 Este paquete almacena las distintas clases que son utilizadas para definir diferentes tipos de interfaces de red
 que tienen soporte dentro del paquete para poder realizar el sniffeo de red.
 
+Del mismo modo, dentro de este paquete se encuentran las distintas cases que son utilizadas para representar las
+unidades de datos utilizadas por diferentes redes para transportar los datos por medio de paquetes de red.
+
 """
