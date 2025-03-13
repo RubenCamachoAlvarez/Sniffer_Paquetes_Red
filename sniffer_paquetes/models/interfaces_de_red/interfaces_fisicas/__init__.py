d0f97a28d1d991d43e8047558333c3f5cfcc4b17
RubenCA <ruben1000.ca@gmail.com>
2025-03-12 21:14:50 -0600

    docs: Documentacion sobre el paquete interfaces_fisicas

---
+++ sniffer_paquetes/models/interfaces_de_red/interfaces_fisicas/__init__.py
@@ -0,0 +1,5 @@
+"""
+Este paquete contiene los subpaquetes donde se encuentran las clases que representan diferentes tipos de interfaces de red fisicas de acuerdo a diferentes
+estandares de redes de computadoras/comunicación.
+
+"""
