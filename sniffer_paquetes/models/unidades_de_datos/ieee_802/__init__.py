e8d8744676fdebf146a9ca8b8e3e5e29f537cc84
RubenCA <ruben1000.ca@gmail.com>
2025-03-12 23:05:49 -0600

    docs: Se agregó la documentación sobre la función del pauqete ieee_802 dentro de unidades_de_datos/

---
+++ sniffer_paquetes/models/unidades_de_datos/ieee_802/__init__.py
@@ -0,0 +1,5 @@
+"""
+Este paquete contiene los modulos que definen las clases que representan las distintas tramas MAC (MAC frames) utilizadas por las redes
+construidos con base en estandares de la IEEE 802.
+
+"""
