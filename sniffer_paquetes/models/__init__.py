a51a0d2f33274520f570e6d91e9629282a88d305
RubenCA <ruben1000.ca@gmail.com>
2025-03-12 20:34:34 -0600

    docs: Se agregó una breve descripción sobre el mótivo del paquete models

---
+++ sniffer_paquetes/models/__init__.py
@@ -0,0 +1,5 @@
+"""
+Este paquete almacena las distintas clases que son utilizadas para definir diferentes tipos de interfaces de red
+que tienen soporte dentro del paquete para poder realizar el sniffeo de red.
+
+"""
