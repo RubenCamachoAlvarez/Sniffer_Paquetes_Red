3ce036d7cea53d348262dc6eff4aa1c7deba9c43
RubenCA <ruben1000.ca@gmail.com>
2025-03-12 21:09:40 -0600

    docs: Se documento el proposito del paquete interfaces_de_red

---
+++ sniffer_paquetes/models/interfaces_de_red/__init__.py
@@ -0,0 +1,5 @@
+"""
+Este paquete contiene la superclase InterfazRed que declara los atributos y metodos abstractos que las subclases se encargaran de definir para llevar
+a cabo el proceso de sniffeo de los paquetes de red de acuerdo a las caracteristicas del tipo de interfaz de red que representen.
+
+"""
