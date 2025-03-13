eda1670e9905cb961c5a297a1665955cb7c0c050
RubenCA <ruben1000.ca@gmail.com>
2025-03-12 22:11:07 -0600

    docs: Documentacion sobre el contenido del paquete unidades_de_datos.

---
+++ sniffer_paquetes/models/unidades_de_datos/__init__.py
@@ -0,0 +1,6 @@
+"""
+
+Este paquete contiene la clases que modelan las distintas unidades de datos utilizadas por la capa de enlace de datos de las redes para
+transportar la informacion dentro de paquetes.
+
+"""
