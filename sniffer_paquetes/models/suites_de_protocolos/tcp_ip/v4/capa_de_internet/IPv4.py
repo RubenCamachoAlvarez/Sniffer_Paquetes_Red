548679e7332da4d3fb71deee88868ec49006915b
RubenCA <ruben1000.ca@gmail.com>
2025-03-14 01:25:29 -0600

    feat: Modulo que contiene la clase que representa un datagrama de internet para la version 4 del protocolo IP.

---
+++ sniffer_paquetes/models/suites_de_protocolos/tcp_ip/v4/capa_de_internet/IPv4.py
@@ -0,0 +1,64 @@
+
+class DatagramaIPv4:
+
+    """
+    Esta clase es utilizada para representar un datagrama de internet en la version 4 de la suite de protocolos TCP/IP.
+
+    De manera análoga a las clases que modelan las distintas unidades de datos a nivel de capa de enlace de datos. Esta clase representa el
+    PDU (protocol data unit) correspondiente en la capa de internet de acuerdo al modelo TCP/IP (modelo DoD).
+
+    En general, de acuerdo a este modelo, en esta capa el PDU recibe el nombre de datagrama de internet.
+
+    Campos del datagrama de internet.
+
+        -Version -> 4 bits.
+        -IHL (Internet Header Length/Longitud del Encabezado de Internet) -> 4 bits.
+        -Tipo de servicio -> 8 bits.
+        -Longitud total -> 16 bits.
+        -Identificador -> 16 bits.
+        -Flags -> 3 bits.
+        -Fragment offset -> 13 bits.
+        -TTL (Time To Live) -> 8 bits.
+        -Protocolo -> 8 bits.
+        -Checksum encabezado -> 16 bits.
+        -Direccion de origen -> 32 bits.
+        -Direccion destino -> 32 bits.
+        -Opciones -> Tamaño de bits variable que deben de ser múltiplos de 4.
+        -Padding -> Tamaño de bits variable que es utilizado para conseguir una alineacion de 32 bits (16 bytes). Los bits que sirven de padding tienen estado 0.
+        -Datos -> octetos del protocolo de nivel superior transportado dentro del datagrama de internet.
+
+    """
+
+    def __init__(self, raw_octets_internet_datagram):
+
+        self.version = None
+
+        self.longitud_encabezado_internet = None
+
+        self.tipo_de_servicio = None
+
+        self.longitud_total = None
+
+        self.identificador = None
+
+        self.flags = None
+
+        self.fragment_offset = None
+
+        self.ttl = None
+
+        self.protocolo = None
+
+        self.checksum_encabezado = None
+
+        self.direccion_origen = None
+
+        self.direccion_destino = None
+
+        self.opciones = None
+
+        #self.padding = None Este campo del encabezado no tiene mucho sentido almacenarlo.
+
+        self.datos = None
+
+
