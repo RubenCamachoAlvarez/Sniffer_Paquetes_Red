0ec9253457c92b3acc57f01a490ec9abed2d737b
RubenCA <ruben1000.ca@gmail.com>
2025-03-14 13:07:50 -0600

    Refactor: La clase TramaMacEthernet ahora es un subtipo de la clase PDU y del mismo modo ahora define el método imprimir_informacion() para que un objeto de esta clase que representa a una trama de Ethernet sea capaz de mostrar rapidamente el valor de los campos que componen a la trama.

--- sniffer_paquetes/models/unidades_de_datos_de_protocolo/unidades_de_datos/ieee_802/ieee_802_3_mac_frame.py
+++ sniffer_paquetes/models/unidades_de_datos_de_protocolo/unidades_de_datos/ieee_802/ieee_802_3_mac_frame.py
@@ -1,5 +1,7 @@
 
-class TramaMacEthernet:
+from ...pdu import PDU
+
+class TramaMacEthernet(PDU):
 
     def __init__(self, raw_octets_trama_mac : bytes):
 
@@ -43,6 +45,10 @@ class TramaMacEthernet:
 
         """
 
+        #Inicializacion del PDU.
+
+        super().__init__()
+
         self.direccion_destino = None
 
         self.direccion_origen = None
@@ -50,6 +56,8 @@ class TramaMacEthernet:
         self.longitud_tipo = None
 
         self.datos = None
+
+        self.es_trama_ieee_802_3 = None
 
         self._procesar_octetos_trama_mac(raw_octets_trama_mac)
 
@@ -65,11 +73,42 @@ class TramaMacEthernet:
 
         """
 
-        self.direccion_destino = ":".join(f"{octeto:02x}" for octeto in raw_octets[:6])
+        self.direccion_destino = ":".join(f"{octeto:02x}" for octeto in raw_octets[:6]).upper()
 
-        self.direccion_origen = ":".join(f"{octeto:02x}" for octeto in raw_octets[6:12])
+        self.direccion_origen = ":".join(f"{octeto:02x}" for octeto in raw_octets[6:12]).upper()
 
         self.longitud_tipo = int.from_bytes(raw_octets[12:14], byteorder="big")
 
-        self.datos = raw_octets[14:] 
+        self.datos = raw_octets[14:]
+
+        #Si el valor transportado en el tercer campo es un valor numérico mayor a 1536, entonces
+
+        if self.longitud_tipo >= 1536:
+
+            #La trama recibida NO corresponde a una trama Ethernet IEEE 802.3 sino a una trama Ethernet II.
+
+            self.es_trama_ieee_802_3 = False
+
+        else:
+
+            self.es_trama_ieee_802_3 = True
+
+
+    def imprimir_informacion(self):
+
+        """Redefinición del método abstracto del PDU para que el usuario del modulo pueda visualizar rapidamente el contenido
+        de los campos de la Trama de Ethernet, excluyendo la interpretación del payload."""
+
+        print(f"Direccion MAC destino: {self.direccion_destino}")
+
+        print(f"Direccion MAC origen: {self.direccion_origen}")
+
+        print(f"Tipo de MAC frame: {"Trama IEEE 802.3" if self.es_trama_ieee_802_3 else "Trama Ethernet II"}")
+
+        print(f"{"Longitud:" if self.es_trama_ieee_802_3 else "Tipo:"} {self.longitud_tipo} {"octetos" if self.es_trama_ieee_802_3 else f"= {hex(self.longitud_tipo)} <- EtherType"}")
+
+        if not self.es_trama_ieee_802_3:
+
+            print(f"Numero de octetos recibidos como payload: {len(self.datos)}")
+
 
