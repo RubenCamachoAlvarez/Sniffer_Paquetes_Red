91282984449a1cf70959ea9bdad532774a14abe7
RubenCA <ruben1000.ca@gmail.com>
2025-03-14 23:03:19 -0600

    feat: El archivo __main__.py ahora contiene el código necesario para llevar a cabo escaneo de tráfico de red en la capa más baja en la que opere la interfaz seleccionada. Este código es puramente de prueba y potencialmente será reemplazado en el corto plazo.

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,25 @@
+from .io_manager.io_cli import seleccionar_interfaz_red
+
+from sniffer_paquetes.models.unidades_de_datos_de_protocolo.pdu import PDU
+
+import sys
+
+def callback(pdu : PDU):
+
+    pdu.imprimir_informacion()
+
+    print("--------------------------------------")
 
 if __name__ == "__main__":
 
-    print("EN PROCESO DE CONSTRUCCIÓN")
+    interfaz_escaneo = seleccionar_interfaz_red()
 
-    
+    if interfaz_escaneo != None:
+
+        print(f"\nInterfaz escogida: {interfaz_escaneo.nombre}\n")
+
+        interfaz_escaneo.capturar_pdu(numero_capturas=10, accion=callback )
+
+    else:
+
+        print(f"No se han detectado interfaces compatibles que tengan soporte en el proyecto actualmente para poder capturar paquetes de red", file=sys.stderr)
