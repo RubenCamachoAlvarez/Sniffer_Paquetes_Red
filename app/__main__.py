51228dcc02dd6b803743be8252b866cf9dc55367
RubenCA <ruben1000.ca@gmail.com>
2025-03-18 04:11:00 -0600

    feat: El script principal de ejecución de la aplicación fue renovado en su totalidad y ahora hace uso de la función run_app() definida dentro del módulo 'core' para comenzar la ejecución del programa CLI.

--- app/__main__.py
+++ app/__main__.py
@@ -1,31 +1,5 @@
-from .io_manager.io_cli import seleccionar_interfaz_red, mostrar_banner_inicio
-
-from sniffer_paquetes.models.unidades_de_datos_de_protocolo.pdu import PDU
-
-import sys
-
-import time
-
-def callback(pdu : PDU):
-
-    pdu.imprimir_informacion()
-
-    print("\n═════════════════════════════════════════════════════\n")
+from .core import run_app
 
 if __name__ == "__main__":
 
-    mostrar_banner_inicio()
-
-    time.sleep(1)
-
-    interfaz_escaneo = seleccionar_interfaz_red()
-
-    if interfaz_escaneo != None:
-
-        time.sleep(1)
-
-        interfaz_escaneo.capturar_pdu(numero_capturas=10, accion=callback )
-
-    else:
-
-        print(f"No se han detectado interfaces compatibles que tengan soporte en el proyecto actualmente para poder capturar paquetes de red", file=sys.stderr)
+    run_app()
