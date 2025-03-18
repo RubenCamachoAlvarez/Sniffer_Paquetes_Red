cce8229338c212daf97fd329c56fd3893c2ff64d
RubenCA <ruben1000.ca@gmail.com>
2025-03-17 22:35:43 -0600

    refactor: Ahora la función seleccionar_interfaz_red() muestra una lista de las interfaces que no tienen soporte previo a solicitar al usuario seleccionar la interfaz correspondiente a utilizar.

--- app/io_manager/io_cli.py
+++ app/io_manager/io_cli.py
@@ -13,34 +13,43 @@ def seleccionar_interfaz_red():
     escoja con cual de estas interfaces es con la que desea realizar el escaneo de los paquetes de red que transitan
     por la red a la que se encuentra conectado su sistema por medio de la interfaz de red seleccionada."""
 
-    interfaces = listar_interfaces_red()
+    interfaces_soportadas, interfaces_no_soportadas = listar_interfaces_red()
 
-    if len(interfaces) > 0:
+    if len(interfaces_no_soportadas) > 0:
+
+        print("\nINTERFACES DE RED NO SOPORTADAS\n", file=sys.stderr)
+
+        for nombre_interfaz in interfaces_no_soportadas:
+
+            print(f"-> {nombre_interfaz}", file=sys.stderr)
+
+
+    if len(interfaces_soportadas) > 0:
 
         while(True):
 
             try:
 
-                print("---------------------------------------------------------------")
+                print("\n---------------------------------------------------------------")
 
-                print("\nMENÚ DE INTERFACES DE RED\n")
+                print("\nMENÚ DE INTERFACES DE RED SOPORTADAS\n")
 
-                for indice_interfaz in range(len(interfaces)):
+                for indice_interfaz in range(len(interfaces_soportadas)):
 
-                    print(f"{indice_interfaz} -> {interfaces[indice_interfaz].nombre}")
+                    print(f"{indice_interfaz} -> {interfaces_soportadas[indice_interfaz].nombre}")
 
                 indice_seleccion = int(input("\nDigita el entero de la interfaz que deseas utilizar: "))
 
-                if 0 <= indice_seleccion < len(interfaces):
+                if 0 <= indice_seleccion < len(interfaces_soportadas):
 
                     break
 
-                print("\nIngresa un entero que corresponda a una interfaz mostrada en el menú.\n", file=sys.stderr)
+                print("\nIngresa un entero que corresponda a una interfaz mostrada en el menú.", file=sys.stderr)
 
             except ValueError:
 
-                print("\nError, ingrese un número entero.\n", file=sys.stderr)
+                print("\nError, ingrese un número entero.", file=sys.stderr)
 
-    return interfaces[indice_seleccion] if len(interfaces) > 0 else None
+    return interfaces_soportadas[indice_seleccion] if len(interfaces_soportadas) > 0 else None
 
         
