af8e35ecc27bb2a70d75015b57360e2cf32a3b47
RubenCA <ruben1000.ca@gmail.com>
2025-03-18 00:57:48 -0600

    refactor: Cambios en el script principal.

--- app/__main__.py
+++ app/__main__.py
@@ -1,22 +1,30 @@
-from .io_manager.io_cli import seleccionar_interfaz_red, modo_no_canonico
+from .io_manager.io_cli import seleccionar_interfaz_red, mostrar_banner_inicio
 
 from sniffer_paquetes.models.unidades_de_datos_de_protocolo.pdu import PDU
 
 import sys
 
+import time
+
 def callback(pdu : PDU):
 
     pdu.imprimir_informacion()
 
-    print("----------------------------------------------")
+    print("\n═════════════════════════════════════════════════════\n")
 
 if __name__ == "__main__":
+
+    mostrar_banner_inicio()
+
+    time.sleep(1)
 
     interfaz_escaneo = seleccionar_interfaz_red()
 
     if interfaz_escaneo != None:
 
         print(f"\nInterfaz escogida: {interfaz_escaneo.nombre}\n")
+
+        time.sleep(1)
 
         interfaz_escaneo.capturar_pdu(numero_capturas=10, accion=callback )
 
